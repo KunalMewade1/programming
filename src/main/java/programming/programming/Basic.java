102f6c2b4d195c70afdf2acb2cf3a72cc1df5eaf
kunalMewade1 <mewade.kunal21@gmail.com>
2024-04-14 18:42:18 +0530

    changes

---
+++ src/main/java/programming/programming/Basic.java
@@ -0,0 +1,11 @@
+package programming.programming;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Basic");
+
+	}
+
+}
